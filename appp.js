39b327d82213c04cd0da237b37c19ec40630449f
hongandepzzi <94431987+hongandepzzi@users.noreply.github.com>
2023-08-13 13:18:09 +0700

    Add files via upload

---
+++ appp.js
@@ -0,0 +1,16 @@
+alert("Đây là trang web của nhà Má Chi\nÈn gioi😛")
+function mixFunction() {
+    alert("Đây là Má Chi")
+}
+function muFunction() {
+    alert("Đay là Ba Dũng")
+}
+function miFunction() {
+    alert("Đây là Chị Bo")
+}
+function moFunction() {
+    alert("Đây là Anh Bill")
+}
+function mcFunction() {
+    alert("Đây là Chị Trang")
+}
